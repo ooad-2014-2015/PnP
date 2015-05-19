e3b00abda15e82f92aad59479af3bf5d532f21fb
Mirnes Peljto <mpeljto1@etf.unsa.ba>
2015-05-19 20:58:10 +0200

    Prepravka

---
+++ Auto-Servis/Auto Servis/Auto Servis/Models/Klijent.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+
+namespace Auto_Servis.Models
+{
+    public class Klijent : INotifyPropertyChanged
+    {
+        public Klijent()
+        {
+
+        }
+        private string ime;
+        public string Ime
+        {
+            get { return ime; }
+            set { ime = value; OnPropertyChanged("Ime"); }
+        }
+
+        private string prezime;
+        public string Prezime
+        {
+            get { return prezime; }
+            set { prezime = value; OnPropertyChanged("Prezime"); }
+        }
+
+        private string adresa;
+        public string Adresa
+        {
+            get { return adresa; }
+            set { adresa = value; OnPropertyChanged("Adresa"); }
+        }
+
+        private int brojTelefona;
+        public int BrojTelefona
+        {
+            get { return brojTelefona; }
+            set { brojTelefona = value; OnPropertyChanged("BrojTelefona"); }
+        }
+
+        Vozilo vozilo;
+        internal Vozilo Vozilo
+        {
+            get { return vozilo; }
+            set { vozilo = value; OnPropertyChanged("Vozilo"); }
+        }
+
+        Zahtjev zahtjev;
+        internal Zahtjev Zahtjev
+        {
+            get { return zahtjev; }
+            set { zahtjev = value; OnPropertyChanged("Zahtjev"); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+    }
+}
